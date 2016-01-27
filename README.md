6fecbca1c90e22e0d8f2f9842b4ab54aa0224eb5
Yura <vid@pasokon.com>
2016-01-28 01:04:49 +0200

    Refactor factory. Added example screenshot.

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
 # GitHubAPI Angular
 Overview
 
-Build a small browser based application which uses GitHub's API to show the repositories for a given user. 
+A small browser based application which uses GitHub's API to show the repositories for a given user. 
 
 Requirements
 - The application should take GitHub usernames as input
 - If one name is supplied, list the repositories belonging to that user, ordered by the number of stars on their own repos
 - If two or more names are supplied, tell us who has the most stars on their own repos
 - Add any additional info you think may be useful to see
+
+
+Example of use:
+![alt tag](http://i.imgur.com/Tagchff.png)
--- js/app.js
+++ js/app.js
@@ -8,161 +8,152 @@
    /*
    * Service for initialization, storing info, and interacting with user data
    */
-   angular.module('Services',[]).factory('githubService',function($rootScope,$http){
-      $rootScope.users = [];
-      this.reposFound = false;
+   angular.module('Services',[]).factory('GithubService',function($rootScope,$http){
 
-      /* 
-      * Method to fetch main information about user.
-      * After success fetch user repositories.
-      */
-      var userRequest = function(username, newNames) {
-         return $http({
-            method: "GET",
-            url: "https://api.github.com/users/" + username
-         })
-         .success(function (data) {
-            // Yes. Name could be unavailable.
-            if (data.name == null) data.name = data.login;
-            /*Call repositories request*/
-            reposRequest(username,data);
-         })
-         .error(function (err,status) {
-            console.log(err,status)
-            // Push 'Not-Found username' just to show it in UI
-            $rootScope.users.push({
-               username,
-               available: false,
-               stargazers: -1,
-               repos: [],
-               err: status == 403 ? 'Query Limit Was Reached' : 'No Such User Info Found'
+      var service = {
+         users: [],
+         reposFound: false,
+         doNotInteract: false,
+
+
+         /* 
+         * Method to fetch main information about user.
+         * After success fetch user repositories.
+         */
+         userRequest: function(username, newNames) {
+            return $http({
+               method: "GET",
+               url: "https://api.github.com/users/" + username
+            })
+            .success(function (data) {
+               // Yes. Name could be unavailable.
+               if (data.name == null) data.name = data.login;
+               /*Call repositories request*/
+               service.reposRequest(username,data);
+            })
+            .error(function (err,status) {
+               console.log(err,status)
+               // Push 'Not-Found username' just to show it in UI
+               service.users.push({
+                  username,
+                  available: false,
+                  stargazers: -1,
+                  repos: [],
+                  err: status == 403 ? 'Query Limit Was Reached' : 'No Such User Info Found'
+               });
+
+               service.uiReload();
             });
 
-            uiReload();
-         });
+         },
 
-      };
+         /**
+         * Method to fetch the list of user repositories
+         * As API said it could show maximum 100 repositories by one request. 
+         * Therefore, to receive all repos - should implement pagination on UI 
+         * or implement query loop while get all repos.
+         */
+         reposRequest: function(username,user_data) {
+            var self = this;
+            return $http({
+               method: "GET",
+               url: "https://api.github.com/users/" + username + "/repos?per_page=100" 
+            })
+            .success(function(data){
+               var starsCount = 0;
 
-      /**
-      * Method to fetch the list of user repositories
-      * As API said it could show maximum 100 repositories by one request. 
-      * Therefore, to receive all repos - should implement pagination on UI 
-      * or implement query loop while get all repos.
-      */
-      var reposRequest = function(username,user_data) {
-         var self = this;
-         return $http({
-            method: "GET",
-            url: "https://api.github.com/users/" + username + "/repos?per_page=100" 
-         })
-         .success(function(data){
-            var starsCount = 0;
+               service.reposFound = data.length > 0;
 
-            self.reposFound = data.length > 0;
+               // If repositories available: count its stars
+               if ( service.reposFound ) {
+                  angular.forEach(data, function(i,index){
+                     starsCount += i.stargazers_count;
+                  })
+               }
 
-            // If repositories available: count its stars
-            if ( self.reposFound ) {
-               angular.forEach(data, function(i,index){
-                  starsCount += i.stargazers_count;
-               })
-            }
+               // Push new git user to array
+               service.users.push({
+                  username,
+                  user_data,
+                  repos: data,
+                  stargazers: starsCount,
+                  available: true
+               });
 
-            // Push new git user to array
-            $rootScope.users.push({
-               username,
-               user_data,
-               repos: data,
-               stargazers: starsCount,
-               available: true
+               service.uiReload();
+
+            })
+            .error(function(err,status){
+               console.log(err,status)
             });
 
-            uiReload();
-
-         })
-         .error(function(err,status){
-            console.log(err,status)
-         });
-
-      };
-
-      /*
-      * When added new username aka $tag
-      */
-      var addNewUser = function() {
-         // Needed to not call service
-         $rootScope.doNotInteract = false;
-         return;
-      }
-
-
-      /*
-      * When delete username aka $tag
-      */
-      var deleteUser = function (tag_name) {
-         $rootScope.doNotInteract = true;
-
-         // var users = $rootScope.users;
-         var deleteUser = tag_name.user_name;
-
-         angular.forEach($rootScope.users,function (i,index) {
-            if ( $rootScope.users[index].username === deleteUser ) {
-               $rootScope.users.splice(index,1)
-               return;
-            }
-
-         })
-
-         uiReload()
-
-      }
-
-      // DOM manipulation
-      var uiReload = function() {
-         setTimeout(function(){
-            jQuery('.hoverable').removeClass('first');
-            jQuery('.hoverable:first').addClass('first');
-            jQuery('.user-block').addClass('col-md-3') 
-            jQuery('.user-block:first').removeClass('col-md-3') 
-         },50)
-
-      }
-
-
-      return {
-         getUsersInfo: function(username, newNames) {
-            return userRequest(username, newNames);
          },
-         getUserRepos: function(username) {
-            return reposRequest(username);
-         },
-         uiReload: function() {
-            return uiReload();
-         },
+
+         /*
+         * When added new username aka $tag
+         */
          addNewUser: function() {
-            return addNewUser();
+            // Needed to not call service
+            service.doNotInteract = false;
+            return;
          },
-         deleteUser: function(tag_name) {
-            return deleteUser(tag_name);
+
+
+         /*
+         * When delete username aka $tag
+         */
+         deleteUser: function (tag_name) {
+            service.doNotInteract = true;
+
+            // var users = $rootScope.users;
+            var deleteUser = tag_name.user_name;
+
+            angular.forEach(service.users,function (i,index) {
+               if ( service.users[index].username === deleteUser ) {
+                  service.users.splice(index,1)
+                  return;
+               }
+
+            })
+
+            service.uiReload()
+
+         },
+
+         // DOM manipulation
+         uiReload: function() {
+            setTimeout(function(){
+               jQuery('.hoverable').removeClass('first');
+               jQuery('.hoverable:first').addClass('first');
+               jQuery('.user-block').addClass('col-md-3') 
+               jQuery('.user-block:first').removeClass('col-md-3') 
+            },50)
+
          }
-      }
+
+      };
+
+
+      return service;
+     
    });
    
    /*
    * GitHubController responsible for assigning service to local scope 
    * and write watcher for users aka $tags collection.
    */
-   app.controller('GitHubController',function($scope, githubService){
+   app.controller('GitHubController',function($scope, GithubService){
 
-      $scope.gitService = githubService;
+      $scope.gitService = GithubService;
 
       // Watcher for inputed users
       $scope.$watchCollection('username', function(newNames,oldNames) { 
 
-         if (newNames && newNames.length >= 1 && !$scope.doNotInteract) {
+         if (newNames && newNames.length >= 1 && !GithubService.doNotInteract) {
             var currentUser = newNames[newNames.length-1].user_name;
             
             // Call service to fetch data
-            githubService.getUsersInfo(currentUser, newNames);
+            GithubService.userRequest(currentUser, newNames);
 
          }
 
